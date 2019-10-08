7dc1a028f2507bdc91e325fa2cee375edbaa38b2
kat-star <katherinewkwong@gmail.com>
2019-10-07 22:51:18 -0700

    Done.

---
+++ models/piglatinizer.rb
@@ -0,0 +1,24 @@
+class PigLatinizer
+  attr_reader :text
+
+  def initialize
+  
+  end
+
+  def piglatinize(text)
+    words = text.split
+
+    words.map do |word|
+      if word =~ /^[aeiou]/i 
+       "#{word}way"
+      else
+        next_vow = word =~ /[aeiou]/i 
+        consonants = word.slice(0...next_vow)
+        rest = word.slice(next_vow..-1)
+        "#{rest}#{consonants}ay"
+      end
+    end.join(' ')
+
+  end
+
+end
